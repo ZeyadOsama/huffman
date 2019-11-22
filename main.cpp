e5db1a22343ca7d1cfb2b8fa568ac8bc4c83864a
ZeyadOsama <zeyad.osama@gmail.com>
2019-11-22 14:39:01 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+///
+/// main.c
+/// Huffman
+///
+/// @author Zeyad Osama.
+/// @date 2019-11-22.
+/// @copyright © 2019 Zeyad Osama. All rights reserved.
+///
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+}
